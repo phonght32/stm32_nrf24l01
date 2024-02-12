d09ff525ea221d27516b9bcc4533bfb0359b60d9
phonght32 <thanhphongho1998@gmail.com>
2024-02-12 20:40:14 +0700

    update main

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,27 +18,64 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "spi.h"
+#include "usart.h"
+#include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include "stdio.h"
 #include "nrf24l01.h"
+#include "serial_log.h"
 /* USER CODE END Includes */
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
 /* USER CODE END PTD */
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define NRF24L01_TX_SPI                 &hspi1
+#define NRF24L01_TX_SPI_CS_PORT         GPIOB
+#define NRF24L01_TX_SPI_CS_PIN_NUM      GPIO_PIN_7
+#define NRF24L01_TX_SPI_CE_PORT         GPIOB
+#define NRF24L01_TX_SPI_CE_PIN_NUM      GPIO_PIN_6
+#define NRF24L01_TX_IRQ_PORT            GPIOB
+#define NRF24L01_TX_IRQ_PIN_NUM         GPIO_PIN_8
+
+#define NRF24L01_RX_SPI                 &hspi2
+#define NRF24L01_RX_SPI_CS_PORT         GPIOC
+#define NRF24L01_RX_SPI_CS_PIN_NUM      GPIO_PIN_4
+#define NRF24L01_RX_SPI_CE_PORT         GPIOC
+#define NRF24L01_RX_SPI_CE_PIN_NUM      GPIO_PIN_5
+#define NRF24L01_RX_IRQ_PORT            GPIOA
+#define NRF24L01_RX_IRQ_PIN_NUM         GPIO_PIN_6
+
+#define HW_SERIAL_LOG_UART_HANDLE       huart4
 /* USER CODE END PD */
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
 /* USER CODE END PM */
 /* Private variables ---------------------------------------------------------*/
 /* USER CODE BEGIN PV */
+nrf24l01_handle_t nrf24l01_tx_handle;
+nrf24l01_handle_t nrf24l01_rx_handle;
+uint8_t tx_data[8] = {'0', '0', '0', '0', '0', '0', '0', '0'};
+uint8_t rx_data[8] = {'0', '0', '0', '0', '0', '0', '0', '0'};
+uint8_t log_buf[50];
 /* USER CODE END PV */
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-static void MX_GPIO_Init(void);
 /* USER CODE BEGIN PFP */
+err_code_t hw_intf_nrf24l01_tx_spi_send(uint8_t *buf_send, uint16_t len, uint32_t timeout_ms);
+err_code_t hw_intf_nrf24l01_tx_spi_recv(uint8_t *buf_recv, uint16_t len, uint32_t timeout_ms);
+err_code_t hw_intf_nrf24l01_tx_set_cs(uint8_t level);
+err_code_t hw_intf_nrf24l01_tx_set_ce(uint8_t level);
+
+err_code_t hw_intf_nrf24l01_rx_spi_send(uint8_t *buf_send, uint16_t len, uint32_t timeout_ms);
+err_code_t hw_intf_nrf24l01_rx_spi_recv(uint8_t *buf_recv, uint16_t len, uint32_t timeout_ms);
+err_code_t hw_intf_nrf24l01_rx_set_cs(uint8_t level);
+err_code_t hw_intf_nrf24l01_rx_set_ce(uint8_t level);
+
+void hw_intf_log_func(uint8_t *data, uint16_t len, uint32_t timeout_ms);
 /* USER CODE END PFP */
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
@@ -62,12 +99,74 @@ int main(void)
     /* USER CODE END SysInit */
     /* Initialize all configured peripherals */
     MX_GPIO_Init();
-  /* USER CODE BEGIN 2 */
-  /* USER CODE END 2 */
+    MX_SPI1_Init();
+    MX_SPI2_Init();
+    MX_UART4_Init();
+    /* USER CODE BEGIN 2 */
+    nrf24l01_tx_handle = nrf24l01_init();
+    nrf24l01_cfg_t nrf24l01_tx_cfg = {
+        .channel = 2500,
+        .crc_len = 1,
+        .addr_width = 5,
+        .retrans_cnt = 3,
+        .retrans_delay = 250,
+        .data_rate = NRF24L01_DATA_RATE_1Mbps,
+        .output_pwr = NRF24L01_OUTPUT_PWR_0dBm,
+        .mode = NRF24L01_MODE_TRANSMITTER,
+        .spi_send = hw_intf_nrf24l01_tx_spi_send,
+        .spi_recv = hw_intf_nrf24l01_tx_spi_recv,
+        .set_cs = hw_intf_nrf24l01_tx_set_cs,
+        .set_ce = hw_intf_nrf24l01_tx_set_ce
+    };
+    nrf24l01_set_config(nrf24l01_tx_handle, nrf24l01_tx_cfg);
+    nrf24l01_config(nrf24l01_tx_handle);
+
+    nrf24l01_rx_handle = nrf24l01_init();
+    nrf24l01_cfg_t nrf24l01_rx_cfg = {
+        .channel = 2500,
+        .crc_len = 1,
+        .addr_width = 5,
+        .retrans_cnt = 3,
+        .retrans_delay = 250,
+        .data_rate = NRF24L01_DATA_RATE_1Mbps,
+        .output_pwr = NRF24L01_OUTPUT_PWR_0dBm,
+        .mode = NRF24L01_MODE_RECEIVER,
+        .spi_send = hw_intf_nrf24l01_rx_spi_send,
+        .spi_recv = hw_intf_nrf24l01_rx_spi_recv,
+        .set_cs = hw_intf_nrf24l01_rx_set_cs,
+        .set_ce = hw_intf_nrf24l01_rx_set_ce
+    };
+    nrf24l01_set_config(nrf24l01_rx_handle, nrf24l01_rx_cfg);
+    nrf24l01_config(nrf24l01_rx_handle);
+
+    serial_log_function_set(hw_intf_log_func, HAL_GetTick);
+
+    /* USER CODE END 2 */
     /* Infinite loop */
     /* USER CODE BEGIN WHILE */
     while (1)
     {
+        static uint8_t cnt = 0;
+
+        if (cnt > 8)
+        {
+            cnt = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                tx_data[i] = '0';
+            }
+        }
+
+        for (int i = 0; i < 8; i++)
+        {
+            tx_data[i]++;
+        }
+
+        nrf24l01_transmit(nrf24l01_tx_handle, tx_data);
+
+        cnt++;
+
+        HAL_Delay(100);
         /* USER CODE END WHILE */
         /* USER CODE BEGIN 3 */
     }
@@ -103,8 +202,8 @@ void SystemClock_Config(void)
     }
     /** Initializes the CPU, AHB and APB buses clocks
     */
-    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                                                            |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+                                  | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
     RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
     RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
     RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
@@ -114,21 +213,92 @@ void SystemClock_Config(void)
         Error_Handler();
     }
 }
-/**
-    * @brief GPIO Initialization Function
-    * @param None
-    * @retval None
-    */
-static void MX_GPIO_Init(void)
-{
-/* USER CODE BEGIN MX_GPIO_Init_1 */
-/* USER CODE END MX_GPIO_Init_1 */
-    /* GPIO Ports Clock Enable */
-    __HAL_RCC_GPIOH_CLK_ENABLE();
-/* USER CODE BEGIN MX_GPIO_Init_2 */
-/* USER CODE END MX_GPIO_Init_2 */
-}
 /* USER CODE BEGIN 4 */
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+    if (GPIO_Pin == NRF24L01_RX_IRQ_PIN_NUM)
+    {
+        nrf24l01_receive(nrf24l01_rx_handle, rx_data); // read data when data ready flag is set
+
+        sprintf((char*)log_buf, (const char*)"\r\nTransmitted data: %s", tx_data);
+        HAL_UART_Transmit(&HW_SERIAL_LOG_UART_HANDLE, (uint8_t*)log_buf, 28, 100);
+
+        sprintf((char*)log_buf, (const char*)"\r\nReceived data   : %s", rx_data);
+        HAL_UART_Transmit(&HW_SERIAL_LOG_UART_HANDLE, (uint8_t*)log_buf, 29, 100);
+
+        sprintf((char*)log_buf, (const char*)"\r\n**************************");
+        HAL_UART_Transmit(&HW_SERIAL_LOG_UART_HANDLE, (uint8_t*)log_buf, 28, 100);
+    }
+    else if (GPIO_Pin == NRF24L01_TX_IRQ_PIN_NUM)
+    {
+        nrf24l01_transmit_irq(nrf24l01_tx_handle);
+    }
+    else
+    {
+
+    }
+}
+
+err_code_t hw_intf_nrf24l01_tx_spi_send(uint8_t *buf_send, uint16_t len, uint32_t timeout_ms)
+{
+    HAL_SPI_Transmit(NRF24L01_TX_SPI, buf_send, len, timeout_ms);
+
+    return ERR_CODE_SUCCESS;
+}
+
+err_code_t hw_intf_nrf24l01_tx_spi_recv(uint8_t *buf_recv, uint16_t len, uint32_t timeout_ms)
+{
+    HAL_SPI_Receive(NRF24L01_TX_SPI, buf_recv, len, timeout_ms);
+
+    return ERR_CODE_SUCCESS;
+}
+
+err_code_t hw_intf_nrf24l01_tx_set_cs(uint8_t level)
+{
+    HAL_GPIO_WritePin(NRF24L01_TX_SPI_CS_PORT, NRF24L01_TX_SPI_CS_PIN_NUM, level);
+
+    return ERR_CODE_SUCCESS;
+}
+
+err_code_t hw_intf_nrf24l01_tx_set_ce(uint8_t level)
+{
+    HAL_GPIO_WritePin(NRF24L01_TX_SPI_CE_PORT, NRF24L01_TX_SPI_CE_PIN_NUM, level);
+
+    return ERR_CODE_SUCCESS;
+}
+
+err_code_t hw_intf_nrf24l01_rx_spi_send(uint8_t *buf_send, uint16_t len, uint32_t timeout_ms)
+{
+    HAL_SPI_Transmit(NRF24L01_RX_SPI, buf_send, len, timeout_ms);
+
+    return ERR_CODE_SUCCESS;
+}
+
+err_code_t hw_intf_nrf24l01_rx_spi_recv(uint8_t *buf_recv, uint16_t len, uint32_t timeout_ms)
+{
+    HAL_SPI_Receive(NRF24L01_RX_SPI, buf_recv, len, timeout_ms);
+
+    return ERR_CODE_SUCCESS;
+}
+
+err_code_t hw_intf_nrf24l01_rx_set_cs(uint8_t level)
+{
+    HAL_GPIO_WritePin(NRF24L01_RX_SPI_CS_PORT, NRF24L01_RX_SPI_CS_PIN_NUM, level);
+
+    return ERR_CODE_SUCCESS;
+}
+
+err_code_t hw_intf_nrf24l01_rx_set_ce(uint8_t level)
+{
+    HAL_GPIO_WritePin(NRF24L01_RX_SPI_CE_PORT, NRF24L01_RX_SPI_CE_PIN_NUM, level);
+
+    return ERR_CODE_SUCCESS;
+}
+
+void hw_intf_log_func(uint8_t *data, uint16_t len, uint32_t timeout_ms)
+{
+    HAL_UART_Transmit(&HW_SERIAL_LOG_UART_HANDLE, (uint8_t*)data, len, timeout_ms);
+}
 /* USER CODE END 4 */
 /**
   * @brief  This function is executed in case of error occurrence.
@@ -136,13 +306,13 @@ static void MX_GPIO_Init(void)
   */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
+    /* USER CODE BEGIN Error_Handler_Debug */
+    /* User can add his own implementation to report the HAL error return state */
+    __disable_irq();
+    while (1)
+    {
+    }
+    /* USER CODE END Error_Handler_Debug */
 }
 #ifdef  USE_FULL_ASSERT
 /**
@@ -154,9 +324,9 @@ void Error_Handler(void)
   */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+    /* USER CODE BEGIN 6 */
+    /* User can add his own implementation to report the file name and line number,
+       ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+    /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
